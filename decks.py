d8409d5b326802802c3eefbf80a3ce1bfdbbbcd0
cartdb <129872277+cartdb@users.noreply.github.com>
2024-10-25 08:41:12 -0500

    Delete decks.py

--- decks.py
+++
@@ -1,7 +0,0 @@
-import pygame
-backside = pygame.image.load("png/back-side.png")
-def cardDeck(screen_width, screen_height, screen):
-    x = 0
-    y = screen_height * 0.3
-    cardImg = pygame.transform.scale(backside, (screen_width * 0.1, screen_height * 0.375))
-    screen.blit(cardImg, (x, y))
